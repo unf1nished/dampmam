032f899d758e38b7c832048546de0dea5d355753
unf1nished <unf1nished@users.noreply.github.com>
2017-06-14 09:45:14 +0200

    Create index.php

--- www/exercise-1/index.php
+++ www/exercise-1/index.php
@@ -1,11 +1,11 @@
 <html><body>
 
-<h1><p style="color:#800080">Sparkles are awesome<p/><h1>
+<h1><p style="color:#eea2ad">Sparkles are awesome<p/><h1>
 
-This page is hosted by your dampmam container! How awesome is that!
+This page is hosted by your dampmam container! How awesome is that? Very awesome.
 
 <?
-echo "This is some PHP code! 2+2=".(2+0);
+echo "This is some PHP code! 2+2=".(2+2);
 ?>
 
 </html></body>
